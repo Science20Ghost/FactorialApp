6cd05ceab804151380968cee07f6c20366c5789d
Science20Ghost <66218669+Science20Ghost@users.noreply.github.com>
2020-06-01 03:08:27 +0700

    release v1

---
+++ FactorialApp/FactorialApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FactorialApp
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            double n = -1;
+            double.TryParse(TextInput.Text, out n);
+            if (n == 0)
+                result.Text = $"Результат: {n}";
+
+            if(n < 0)
+            {
+                result.Text = "Введите положительное число";
+                return;
+            }
+
+            double fact = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * (double)i;
+            }
+
+            result.Text = $"результат: {fact}";
+        }
+    }
+}
